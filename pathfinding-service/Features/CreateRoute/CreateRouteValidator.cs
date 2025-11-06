99b05fc58c88479a4ca6d255889c0f4d64d7775c
Kevin <173805497+KevinLabrosse@users.noreply.github.com>
2025-11-06 15:57:06 +0100

    Comment out CreateRouteValidator implementation
    The CreateRouteValidator class and its implementation have been commented out for refactoring. Also fixed namespace casing and updated using directives.

--- pathfinding-service/Features/CreateRoute/CreateRouteValidator.cs
+++ pathfinding-service/Features/CreateRoute/CreateRouteValidator.cs
@@ -1,15 +1,16 @@
 using FluentValidation;
+using PathfindingService.Features.CreateRoute;
 
-namespace pathfindingService.Features.CreateRoute
+namespace PathfindingService.Features.CreateRoute
 {
-    public class CreateRouteValidator : AbstractValidator<CreateRoute>
-    {
-        public CreateRouteValidator()
-        {
-            RuleFor(x => x.Origin).NotEmpty().WithMessage("Origin must be specified");
-            RuleFor(x => x.Destination).NotEmpty().WithMessage("Destination must be specified");
-            RuleFor(x => x.CorrelationId).NotEmpty().WithMessage("CorrelationID is required");
-            RuleFor(x => x.Origin).NotEqual(x => x.Destination).WithMessage("Origin and Destination must be different")
-        }
-    }
+    // public class CreateRouteValidator : AbstractValidator<CreateRouteCommand>
+    // {
+    //     public CreateRouteValidator()
+    //     {
+    //         RuleFor(x => x.Origin).NotEmpty().WithMessage("Origin must be specified");
+    //         RuleFor(x => x.Destination).NotEmpty().WithMessage("Destination must be specified");
+    //         RuleFor(x => x.CorrelationId).NotEmpty().WithMessage("CorrelationID is required");
+    //         RuleFor(x => x.Origin).NotEqual(x => x.Destination).WithMessage("Origin and Destination must be different");
+    //     }
+    // }
 }
