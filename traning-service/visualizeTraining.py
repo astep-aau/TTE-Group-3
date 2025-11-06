8b2d9237e6f365e8140cef553860263386b95428
ESk0v <Emil@sk0v.dk>
2025-11-06 09:39:12 +0100

    Skulle ikke bruges til noget
    Yes yes

--- traning-service/visualizeTraining.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as graph
-
-def plotGraph(epochs, loss_history):
-    graph.plot(range(1, epochs+1), loss_history, marker='o')
-    graph.xlabel('Epoch')
-    graph.ylabel('Loss')
-    graph.title('Training Loss over Epochs')
-    graph.show()
