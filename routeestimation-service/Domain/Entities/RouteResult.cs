d08ec8361b4a649e348769edfae1603c5c1cfc48
Kevin <173805497+KevinLabrosse@users.noreply.github.com>
2025-11-14 10:37:19 +0100

    Improve RouteResult readability and data structure
    Changed EmbeddedEdges to a list of lists for better data representation. Added a custom ToString() override to provide more readable logging output of RouteResult contents.

--- routeestimation-service/Domain/Entities/RouteResult.cs
+++ routeestimation-service/Domain/Entities/RouteResult.cs
@@ -1,13 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RouteEstimationService.Domain.Entities;
 
 public record RouteResult
 {
     public Guid RouteId { get; } = Guid.NewGuid();
-    public List<string> NodeIds { get; init; } = [];
-    public List<int> EdgeIds { get; init; } = [];
-    public List<double> EmbeddedEdges { get; set; } = [];
+    public List<string> NodeIds { get; init; } = new List<string>();
+    public List<int> EdgeIds { get; init; } = new List<int>();
+    public List<List<double>> EmbeddedEdges { get; set; } = new List<List<double>>();
     public double EstimatedTimeSeconds { get; set; } = 0;
+
+    // Provide a readable string so logging prints list contents instead of the collection type name
+    public override string ToString()
+    {
+        string nodes = NodeIds is { Count: > 0 } ? string.Join(", ", NodeIds) : string.Empty;
+        string edges = EdgeIds is { Count: > 0 } ? string.Join(", ", EdgeIds) : string.Empty;
+        string embedded = EmbeddedEdges is { Count: > 0 } ? string.Join("; ", EmbeddedEdges.Select(list => "[" + string.Join(", ", list) + "]")) : string.Empty;
+        return $"RouteResult {{ RouteId = {RouteId}, NodeIds = [{nodes}], EdgeIds = [{edges}], EmbeddedEdges = [{embedded}], EstimatedTimeSeconds = {EstimatedTimeSeconds} }}";
+    }
 }
