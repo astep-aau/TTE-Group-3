f05b14ed2dca1472c67d9e6fb11fa9685ffb1d23
Kevin <173805497+KevinLabrosse@users.noreply.github.com>
2025-11-26 12:54:57 +0100

    Add custom console formatter and Swagger API key support
    Introduces a CustomConsoleFormatter for colorized log output, specifically highlighting '[C# Controller]' messages. Updates Program.cs to register the custom formatter and configures Swagger to require an API key via the 'X-API-Key' header for endpoint access.

--- training-service/C#/Program.cs
+++ training-service/C#/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using TrainingService.Services;
 using TrainingService.Infrastructure;
 
@@ -9,9 +10,42 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<Service>();
 builder.Services.AddSingleton<TrainingQueue>();
 builder.Services.AddHostedService<TrainingWorker>();
+builder.Services.AddScoped<ApiKeyAuthFilter>();
+builder.Logging.AddConsole(options =>
+{
+    options.FormatterName = "customFormatter";
+});
+
+builder.Logging.AddConsoleFormatter<CustomConsoleFormatter, CustomConsoleFormatterOptions>();
+
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "API Key needed to access the endpoints. Enter your API key in the text input below.",
+        In = ParameterLocation.Header,
+        Name = "X-API-Key",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "ApiKeyScheme"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
